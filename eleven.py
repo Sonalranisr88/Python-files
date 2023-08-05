61e419004de9132456e952ddbbe336dd7694e90f
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-05 23:53:49 +0530

    Add files via upload

---
+++ eleven.py
@@ -0,0 +1,45 @@
+# # l=[12,0,34,0,33,21,0]
+
+# # x=l.count(0)
+# # print(x)
+
+# # st=input("enter the string: ")
+# # if(len(st)<=10):
+# #     print("it has 10 and less than 10 characters")
+# # else:
+# #     print("it has mpre than 10 character")
+
+
+# a=input("enter name:")
+# b=input("enter name:")
+# c=input("enter name:")
+# d=input("enter name:")
+# e=input("enter name:")
+
+# l=[a,b,c,d,e]
+# n=input("enter the name to check:")
+
+# if(n in l):
+#     print("name is present")
+# else:
+#     print("name is not present")
+# (a,b,c,d,e)
+
+
+
+
+marks=int(input("ENter your marks:"))
+if(marks>90):
+    print ("your grade is excellent")
+elif(marks>90 ):
+    print ("your grade is A+")
+elif(marks>90 ):
+    print ("your grade is A")   
+elif(marks>80 ):
+    print ("your grade is B")
+elif(marks>70 ):
+    print ("your grade is C")
+elif(marks>60 ):        
+    print ("your grade is D")
+else:
+    print("you are fail")
