cfcbd3432038ff68c0552d4bb786eefe5ab44b6f
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-08 23:55:24 +0530

    Add files via upload

---
+++ ninth.py
@@ -0,0 +1,10 @@
+a=input("enter fruit:")
+b=input("enter fruit:")
+c=input("enter fruit:")
+d=input("enter fruit:")
+e=input("enter fruit:")
+f=input("enter fruit:")
+g=input("enter fruit:")
+
+fruits=[a,b,c,d,e,f,g]
+print(fruits)
