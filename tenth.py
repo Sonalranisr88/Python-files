c819fdbf97a1c6602ca6c686e1055a9daaa2ea24
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-10 23:50:02 +0530

    Add files via upload

---
+++ tenth.py
@@ -0,0 +1,10 @@
+a= int(input("Enter marks: "))
+b= int(input("Enter marks: "))
+c= int(input("Enter marks: "))
+d= int(input("Enter marks: "))
+e= int(input("Enter marks: "))
+f= int(input("Enter marks: "))
+
+marks=[a,b,c,d,e,f]
+marks.sort()
+print(marks)
